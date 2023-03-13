ad54553aee56ff1bd3f5bff7f72de14bc0491881
Jan Erik Petersen <JanErikPetersen@web.de>
2023-03-14 00:47:50 +0100

    Util: Add memoization function, cache result of reflection utils

--- src/Util.php
+++ src/Util.php
@@ -17,9 +17,9 @@ final class Util {
      * Throws an exception if the given parameter is false, otherwise returns it as-is. Can be used to check return
      * values of functions that return a specific value on error instead of throwing an exception.
      *
-     * @param mixed $condition The value to check.
-     * @param mixed $error The value that signals an error, false by default.
-     * @throws RuntimeException If the given value is the error value.
+     * @param mixed $condition The value to check
+     * @param mixed $error The value that signals an error, false by default
+     * @throws RuntimeException If the given value is the error value
      */
     public static function ensure(mixed $condition, mixed $error = false): mixed {
         if ($condition === $error) {
@@ -30,22 +30,73 @@ final class Util {
     }
 
     /**
+     * Memoize a given `$callable`, i.e. cache its result and return the cached results on successive calls with the
+     * same argument. The cache is stored in memory.
+     *
+     * @template P of array-key
+     * @template T of mixed
+     * @param callable(P): T $callable A deterministic callable, i.e. always returns the same for the same argument
+     * @return callable(P): T The callable with memoization
+     */
+    public static function memoize(callable $callable): callable {
+        return static function (mixed $arg) use ($callable) {
+            static $cache = [];
+
+            if (array_key_exists($arg, $cache)) {
+                return $cache[$arg];
+            }
+
+            $result = $callable($arg);
+            $cache[$arg] = $result;
+            return $result;
+        };
+    }
+
+    /**
+     * @var array<string, bool>
+     */
+    private static array $instantiabilityCache = [];
+
+    /**
      * Returns whether the given `$className` denotes an instantiable class, that is, is a class (not an interface), not
      * abstract and has a public constructor.
+     *
+     * Note that this will trigger the autoloader, which may affect performance. Therefore this check should be avoided
+     * unless the class is about to be used anyway.
      *
      * @param string $className A fully-qualified class name
      */
     public static function isInstantiableClass(string $className): bool {
-        return class_exists($className) && (new ReflectionClass($className))->isInstantiable();
+        if (array_key_exists($className, self::$instantiabilityCache)) {
+            return self::$instantiabilityCache[$className];
+        }
+
+        $result = class_exists($className) && (new ReflectionClass($className))->isInstantiable();
+        self::$instantiabilityCache[$className] = $result;
+        return $result;
     }
+
+    /**
+     * @var array<string, bool>
+     */
+    private static array $classTypeCache = [];
 
     /**
      * Returns whether the given `$className` denotes a valid class type, i.e. a class or interface with the given name
      * exists.
      *
+     * Note that this will trigger the autoloader, which may affect performance. Therefore this check should be avoided
+     * unless the type is about to be used anyway.
+     *
      * @param string $className A fully-qualified type name
      */
     public static function isClassType(string $className): bool {
-        return class_exists($className) || interface_exists($className);
+        if (array_key_exists($className, self::$classTypeCache)) {
+            return self::$classTypeCache[$className];
+        }
+
+        $result = class_exists($className) || interface_exists($className);
+        self::$classTypeCache[$className] = $result;
+        return $result;
     }
 }
